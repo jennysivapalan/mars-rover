805debf2dc6b85709771866f022b99dde3f732f6
Jennifer Sivapalan <944375+jennysivapalan@users.noreply.github.com>
2023-11-08 15:43:30 +0000

    test: refactor input to be just minimum data needed for test

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -2,7 +2,7 @@ import { createMars, Mars } from "../src/index";
 
 describe("test taking input commands to create mars rover", () => {
   it("should create a plateau for mars based on input string", () => {
-    const input = ["5 5", "1 2 N", "LMLMLMLMM", "3 3 E", "MMRMMRMRRM"];
+    const input = ["5 5", "1 2 N"];
     const mars = createMars(input);
 
     expect(mars.plateau.width).toBe(5);
@@ -11,28 +11,28 @@ describe("test taking input commands to create mars rover", () => {
 
   it("should error if first line is incorrect ", () => {
     expect(() => {
-      createMars(["5", "1 2 N", "LMLMLMLMM", "3 3 E", "MMRMMRMRRM"]);
+      createMars(["5", "1 2 N"]);
     }).toThrow("Can't create plateau - Error: Invalid parameters, try again");
 
     expect(() => {
-      createMars(["5 2 N", "1 2 N", "LMLMLMLMM", "3 3 E", "MMRMMRMRRM"]);
+      createMars(["5 2 N", "1 2 N"]);
     }).toThrow("Can't create plateau, invalid first line");
 
     expect(() => {
-      createMars(["", "1 2 N", "LMLMLMLMM", "3 3 E", "MMRMMRMRRM"]);
+      createMars(["", "1 2 N"]);
     }).toThrow("Can't create plateau - Error: Invalid parameters, try again");
 
     expect(() => {
-      createMars(["ABC ABC", "1 2 N", "LMLMLMLMM", "3 3 E", "MMRMMRMRRM"]);
+      createMars(["ABC ABC", "1 2 N"]);
     }).toThrow("Can't create plateau - Error: Invalid parameters, try again");
 
     expect(() => {
-      createMars(["-5 4.3", "1 2 N", "LMLMLMLMM", "3 3 E", "MMRMMRMRRM"]);
+      createMars(["-5 4.3", "1 2 N"]);
     }).toThrow("Can't create plateau - Error: Invalid parameters, try again");
   });
 
   it("should create first rover for mars based on input string", () => {
-    const input = ["5 5", "1 2 N", "LMLMLMLMM"];
+    const input = ["5 5", "1 2 N"];
     const mars = createMars(input);
 
     expect(mars.rover.x).toBe(1);
@@ -42,7 +42,7 @@ describe("test taking input commands to create mars rover", () => {
 
   it("should error if the second line is incorrect ", () => {
     expect(() => {
-      createMars(["5 5", "1", "LMLMLMLMM"]);
+      createMars(["5 5", "1"]);
     }).toThrow(
       "Can't create rover - Error: Invalid rover parameters, try again"
     );
