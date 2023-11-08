052fde1940c710963a81cb60ff04193c89a72925
Jennifer Sivapalan <944375+jennysivapalan@users.noreply.github.com>
2023-11-08 14:56:33 +0000

    tests: edge cases for other invalid inputs for first line to stop creating a plateau

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -17,5 +17,17 @@ describe("test taking input commands to create mars rover", () => {
     expect(() => {
       createMars(["5 2 N", "1 2 N", "LMLMLMLMM", "3 3 E", "MMRMMRMRRM"]);
     }).toThrow("Can't create plateau, invalid first line");
+
+    expect(() => {
+      createMars(["", "1 2 N", "LMLMLMLMM", "3 3 E", "MMRMMRMRRM"]);
+    }).toThrow("Can't create plateau - Error: Invalid parameters, try again");
+
+    expect(() => {
+      createMars(["ABC ABC", "1 2 N", "LMLMLMLMM", "3 3 E", "MMRMMRMRRM"]);
+    }).toThrow("Can't create plateau - Error: Invalid parameters, try again");
+
+    expect(() => {
+      createMars(["-5 4.3", "1 2 N", "LMLMLMLMM", "3 3 E", "MMRMMRMRRM"]);
+    }).toThrow("Can't create plateau - Error: Invalid parameters, try again");
   });
 });
